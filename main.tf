bb10ef3eeeb6028e70142f4a7f0d8ffd86d16e9d
Lance Haig <lnhaig@gmail.com>
2019-12-03 13:51:19 +0100

    Update main.tf
    Add some variables

--- main.tf
+++ main.tf
@@ -1,33 +1,49 @@
-provider "vsphere" {
-  user           = "${var.vsphere_user}"
-  password       = "${var.vsphere_password}"
-  vsphere_server = "${var.vsphere_server}"
+variable "prefix" {
+  default = "tfe"
+}
 
+variable "datacenter" {
+  description = "Datacenter Name"
+}
+
+variable "datastore_name" {
+  description = "Datastore Name"
+}
+
+variable "resource_pool" {
+  description = "Resoucre pool Name"
+}
+
+variable "network_name" {
+  description = "Network Name"
+}
+
+provider "vsphere" {
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "dc" {
-  name = "dc1"
+  name = var.datacenter
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "datastore1"
+  name          = var.datastore_name
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = "cluster1/Resources"
+  name          = var.resource_pool
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_network" "network" {
-  name          = "public"
+  name          = var.network_name
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-test"
+  name             = "${var.prefix}-server"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
